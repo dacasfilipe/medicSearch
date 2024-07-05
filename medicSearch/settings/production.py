2154d1e6564acee9e26278ee9b4a43c5faade5bf
dacasfilipe <filiperdacas@gmail.com>
2024-07-04 22:05:06 -0300

    first commit

---
+++ medicSearch/settings/production.py
@@ -0,0 +1,23 @@
+from .settings import *
+DEBUG = True
+#chave secreta
+SECRET_KEY = 'dasdsadasdsad'
+#conectar no banco sqlite3
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.bacends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+#conectar no banco mysql
+# DATABASES = {
+#     'default':{
+#         'ENGINE':'django.db.backends.mysql',
+#         'NAME':'medicdb',
+#         'USER':'root',
+#         'PASSWORD':'root',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+# }
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
